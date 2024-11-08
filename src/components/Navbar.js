4e0a636d3b7e4867fda5b61043f9b7afbecb31bc
Mathankumar N <mathankumarn1104@gmail.com>
2024-11-08 21:30:43 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,41 @@
-function Navbar ()
+// Navbar
+
+function Navbar()
 {
   return(
-      
+    
     <div className="navbar">
-    <div className="navbar__s1">
-     <h1 className="navbar__s1__title">Ûdemy</h1>
-    
-    </div>
 
-    <div className="navbar__s2">
-        <i className="fa-solid fa-magnifying-glass" style={{color: "#000000"}}></i>
-        <input placeholder="Search for anything here.Art,Business,Tech..."></input>
-    </div>
-
-    <div className="navbar__s3">
-     <p>Courses</p>  
-    
-     <div className="MyLearning">
-        <p>My Learning</p>
-        <div className="MyLearning__popup">
-            <p>You did not Purchase anything yet</p>
+        <div className="navbar__s1">
+            <h1 className="navbar_s1_title"> Udemy</h1>
 
         </div>
-     </div>
-     <i className="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
-     <i className="fa-solid fa-bell" style={{color: "#000000"}}></i>
-     <i className="fa-solid fa-user" style={{color: "#000000"}}></i>
 
+        <div className="navbar__s2">
+            <i className="fa-solid fa-magnifying-glass" style={{color: "#000000"}}></i>
+            <input placeholder="Search for anything here.Tech,Business,Art..."></input>
+
+        </div>
+
+        <div className="navbar__s3">
+            <p>Courses</p>
+           
+            <div className="my-learning">
+                <p>My Learning </p>
+                <div className="my-learning__hover">
+                    <p>You Did Not Purchase Anything Yet </p>
+                </div>
+            </div>
+            <i className="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
+            <i className="fa-solid fa-bell" style={{color: "#000000"}}></i>
+            <i className="fa-solid fa-user" style={{color: "#000000"}}></i>
+
+        </div>
+        <div className="navbar__s4">
+            <i className="fa-solid fa-bars" style={{color: "#000000"}}></i>
+        </div>
 
     </div>
-     <div className="navbar__s4">
-        <i className="fa-solid fa-bars"></i>
-     </div>
-    </div>
-
   )
-
-  
 }
-
-export default Navbar
+export default Navbar
